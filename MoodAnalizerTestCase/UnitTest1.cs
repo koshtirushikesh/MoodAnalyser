cea35a782c26519144dcaa931248b0a13af536f3
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-03 10:11:30 +0530

    [Rushi]:Given “I am in Happy Mood” message in Constructor Should Return SAD

--- MoodAnalizerTestCase/UnitTest1.cs
+++ MoodAnalizerTestCase/UnitTest1.cs
@@ -4,17 +4,19 @@ namespace MoodAnalizerTestCase
 {
     public class Tests
     {
-        MoodAnalizerClass mood = new MoodAnalizerClass("I am in Sad Mood");
+        
 
         [Test]
         public void GivenSadMood_WhenAnalized_ShouldReturnSad()
         {
+            MoodAnalizerClass mood = new MoodAnalizerClass("I am in Sad Mood");
             string result = mood.AnalizeMood();
             Assert.AreEqual("Sad", result);
         }
         [Test]
         public void GivenHappyMood_WhenAnalized_ShouldReturnHappy()
         {
+            MoodAnalizerClass mood = new MoodAnalizerClass("I am in Happy Mood");
             string result = mood.AnalizeMood();
             Assert.AreEqual("Sad", result);
         }
