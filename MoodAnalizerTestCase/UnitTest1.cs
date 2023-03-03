321d55ef74ad488a08b919b5b2ef876e1304c886
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-03 09:25:26 +0530

    [Rushi]:Handling Null Exception Test Case

--- MoodAnalizerTestCase/UnitTest1.cs
+++ MoodAnalizerTestCase/UnitTest1.cs
@@ -4,19 +4,28 @@ namespace MoodAnalizerTestCase
 {
     public class Tests
     {
-        MoodAnalizerClass mood = new MoodAnalizerClass("I am in Sad Mood");
+        
 
         [Test]
         public void GivenSadMood_WhenAnalized_ShouldReturnSad()
         {
+            MoodAnalizerClass mood = new MoodAnalizerClass("I am in Sad Mood");
             string result = mood.AnalizeMood();
             Assert.AreEqual("Sad", result);
         }
         [Test]
         public void GivenHappyMood_WhenAnalized_ShouldReturnHappy()
         {
+            MoodAnalizerClass mood = new MoodAnalizerClass("I am in Happy Mood");
             string result = mood.AnalizeMood();
-            Assert.AreEqual("Sad", result);
+            Assert.AreEqual("Happy", result);
+        }
+        [Test]
+        public void GivenNullMood_WhenAnalized_ShouldReturnHappy()
+        {
+            MoodAnalizerClass mood = new MoodAnalizerClass(null);
+            string result = mood.AnalizeMood();
+            Assert.AreEqual("Happy", result);
         }
 
     }
