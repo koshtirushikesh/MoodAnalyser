f7f4a7758ba5535cff47268fc3769f6832a978bd
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-03 08:45:47 +0530

    [Rushi]:given Sad Mood Should return Sad

---
+++ MoodAnalizer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalizer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MoodAnalizerClass mood = new MoodAnalizerClass();
+            mood.AnalizeMood("I am Sad");
+            Console.ReadLine();
+        }
+    }
+}
